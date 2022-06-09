ba8b563b77259f26d597b4a782e9540b79213958
Temistocles Zwang <temis2st@gmail.com>
2022-06-09 10:50:01 -0300

    refact: modificado casos de uso

--- atv_14/lib/features/pokemon_clean_arch/domain/usecases/get_all_pokemon/get_all_pokemon_imp.dart
+++ atv_14/lib/features/pokemon_clean_arch/domain/usecases/get_all_pokemon/get_all_pokemon_imp.dart
@@ -4,28 +4,16 @@ import '../../../../../core/error/failure.dart';
 
 import '../../entities/pokemon_entity.dart';
 import '../../repositories/pokemon_repository.dart';
-import 'get_all_pokemon_usecase.dart';
+import '../pokemon_usecase.dart';
 //.import 'package:rick_and_morty/presentation/features/rick_and_morty/presentation/widgets/character_card.dart';
 
-class GetAllPokemonImp implements GetAllPokemonUsecase {
-  final PokemonRepository _pokemonRepository;
-  GetAllPokemonImp(this._pokemonRepository);
+class GetAllPokemonImp extends UseCase<List<PokemonEntity>, NoParams> {
+  final PokemonRepository repository;
 
-  // @override
-  // Future<dynamic> getAllPokemon() async {
-    // return await _characterRepository.getAllPokemon();
-  // } //.antigo
+  GetAllPokemonImp(this.repository);
 
-    Future<Either<Failure, PokemonEntity>> execute({
-    required int number, //!
-  }) async {
-    return await _pokemonRepository.getAllPokemon();
-  }
-  
   @override
-  Future<List> call() {
-    // TODO: implement call
-    throw UnimplementedError();
+  Future<Either<Failure, List<PokemonEntity>>> call(NoParams params) async {
+    return await repository.getAllPokemon();
   }
 }
-  
