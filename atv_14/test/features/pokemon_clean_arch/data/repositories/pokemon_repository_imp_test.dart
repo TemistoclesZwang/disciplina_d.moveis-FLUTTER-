e0df2569df90419ddc093c86b2c5a531ccdb6675
Temistocles Zwang <temis2st@gmail.com>
2022-06-15 08:28:14 -0300

    Error: teste with no solution

--- atv_14/test/features/pokemon_clean_arch/data/repositories/pokemon_repository_imp_test.dart
+++ atv_14/test/features/pokemon_clean_arch/data/repositories/pokemon_repository_imp_test.dart
@@ -2,6 +2,7 @@ import 'package:atv_14/core/platform/network_info.dart';
 import 'package:atv_14/features/pokemon_clean_arch/data/datasources/local/pokemon_local_data_source.dart';
 import 'package:atv_14/features/pokemon_clean_arch/data/datasources/remote/pokemon_remote_data_source.dart';
 import 'package:atv_14/features/pokemon_clean_arch/data/repositories/pokemon_repository_imp.dart';
+import 'package:atv_14/features/pokemon_clean_arch/domain/entities/pokemon_entity.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:data_connection_checker/data_connection_checker.dart';
@@ -31,4 +32,30 @@ void main() {
       networkInfo: mockNetworkInfo,
     );
   });
+
+  
+    test('should check if the device is online', () async {
+      // arrange
+      when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+
+      
+});
+
+    test('should return remote data when the device is online', () async {
+      // arrange
+      when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+      final testPokemon = PokemonEntity(
+        id: 1,
+        name: 'test',
+      );
+      when(mockRemoteDataSource.GetAllPokemon()).thenAnswer((_) async => [testPokemon]);
+      // act
+      final result = await repository.getAllPokemon();
+      // assert
+      verify(mockRemoteDataSource.GetAllPokemon());
+      expect(result, Right([testPokemon]));
+    }
+
 }
+
+});
