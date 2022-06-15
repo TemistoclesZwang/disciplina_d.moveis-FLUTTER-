811814ef12059a04baad09790d6d6aaac0afccc7
Temistocles Zwang <temis2st@gmail.com>
2022-06-15 09:27:55 -0300

    refact: repo imp test (error)

--- atv_14/test/features/pokemon_clean_arch/data/repositories/pokemon_repository_imp_test.dart
+++ atv_14/test/features/pokemon_clean_arch/data/repositories/pokemon_repository_imp_test.dart
@@ -1,6 +1,7 @@
 import 'package:atv_14/core/platform/network_info.dart';
 import 'package:atv_14/features/pokemon_clean_arch/data/datasources/local/pokemon_local_data_source.dart';
 import 'package:atv_14/features/pokemon_clean_arch/data/datasources/remote/pokemon_remote_data_source.dart';
+import 'package:atv_14/features/pokemon_clean_arch/data/models/pokemon_entity_model.dart';
 import 'package:atv_14/features/pokemon_clean_arch/data/repositories/pokemon_repository_imp.dart';
 import 'package:atv_14/features/pokemon_clean_arch/domain/entities/pokemon_entity.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -16,46 +17,38 @@ class MockLocalDataSource extends Mock implements GetLastPokemonLocalDataSource 
 class MockNetworkInfo extends Mock implements NetworkInfo {}
 
 void main() {
-  PokemonRepositoryImp repository;
-  MockRemoteDataSource mockRemoteDataSource;
-  MockLocalDataSource mockLocalDataSource;
-  MockNetworkInfo mockNetworkInfo;
+    PokemonRepositoryImp repository;
+    MockRemoteDataSource mockRemoteDataSource;
+    MockLocalDataSource mockLocalDataSource;
+    MockNetworkInfo mockNetworkInfo;    
+    
+    setUp(() {
+    final mockRemoteDataSource = MockRemoteDataSource();
+    final mockLocalDataSource = MockLocalDataSource();
+    final mockNetworkInfo = MockNetworkInfo();
+    final repository = PokemonRepositoryImp(
 
-  setUp(() {
-    mockRemoteDataSource = MockRemoteDataSource();
-    mockLocalDataSource = MockLocalDataSource();
-    mockNetworkInfo = MockNetworkInfo();
-
-    repository = PokemonRepositoryImp(
       remoteDataSource: mockRemoteDataSource,
       localDataSource: mockLocalDataSource,
       networkInfo: mockNetworkInfo,
     );
   });
 
-  
-    test('should check if the device is online', () async {
-      // arrange
-      when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+  group('getConcreteNumberTrivia', () {
+  // DATA FOR THE MOCKS AND ASSERTIONS
+  // We'll use these three variables throughout all the tests
+  final tId = 1;
+  final mockNetworkInfo = MockNetworkInfo();
+  final tPokemonModel =
+      PokemonEntityModel( id: tId, name: 'Bulbasaur');
+  final PokemonEntity tPokemonEntity = tPokemonModel;
 
-      
+  test('should check if the device is online', () {
+    //arrange
+  setUp(() {
+        when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+      });
+  });
 });
-
-    test('should return remote data when the device is online', () async {
-      // arrange
-      when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
-      final testPokemon = PokemonEntity(
-        id: 1,
-        name: 'test',
-      );
-      when(mockRemoteDataSource.GetAllPokemon()).thenAnswer((_) async => [testPokemon]);
-      // act
-      final result = await repository.getAllPokemon();
-      // assert
-      verify(mockRemoteDataSource.GetAllPokemon());
-      expect(result, Right([testPokemon]));
-    }
 
 }
-
-});
